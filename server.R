de528d4f25dabe04133ca754921d12dd4c335aa7
sucrepoisson <vyd225@gmail.com>
2016-07-28 20:04:47 -0700

    dashboard, infile, and model updates

--- server.R
+++ server.R
@@ -1,4 +1,4 @@
-setwd("/Users/kate.hu/addhazard_shiny") # to do: finish
+# setwd("/Users/kate.hu/addhazard_shiny") # to do: finish
 # to do: kate
 # practice
 #data<-read.csv("aric.csv", header=TRUE)
@@ -13,15 +13,41 @@ sourceDir <- function(path, trace = TRUE, ...) {
     if(trace) cat("\n")
   }
 }
-sourceDir("/Users/kate.hu/Documents/ah/R",trace=TRUE)
+# sourceDir("/Users/kate.hu/Documents/ah/R",trace=TRUE)
 library(shiny)
 library(xtable)
+library("addhazard")
+library("survival")
+
 shinyServer(function(input, output) {
   
   data  <- reactive({
    inFile <- input$file1
-   dat <-read.csv(inFile$datapath, header=input$header, sep=input$sep, quote=input$quote)
-   #return(colnames(data))
+   
+   k <- nchar(inFile) # length of filepath
+   if (substr(inFile[[1]], k-2, k) == "csv"){
+      dat <- read.csv(inFile$datapath, header=input$header, sep=input$sep, quote=input$quote)
+   
+   } else if (substr(inFile[[1]], k-2, k) == "txt"){
+      dat <- read.table(inFile$datapath, header=input$header, sep=input$sep, quote=input$quote)
+   
+   } else if (substr(inFile[[1]], k-3, k) == "xlsx"){
+      library(xlsx)
+      if (input$header == T){
+         dat <- read.xlsx2(inFile$datapath, sheetIndex=1, startRow=1)
+      } else { 
+         dat <- read.xlsx2(inFile$datapath, sheetIndex=1, startRow=2)
+      } 
+   
+   } else if (substr(inFile[[1]], k-2, k) == "dta"){
+      library(foreign)
+      dat <- read.dta(inFile$datapath)
+         
+   } else if (substr(inFile[[1]], k-2, k) == "sav"){
+      library(memisc)
+      dat <- as.data.set(spss.system.file(inFile$datapath))
+   }
+   
    dat <-data.frame(dat)
    return(dat)
    })
@@ -69,50 +95,100 @@ shinyServer(function(input, output) {
   })
   
   output$R <- renderUI({
-    selectInput("R", label = h5("Phase II membership"), choices = names(data()))
+    selectInput("R", label = h5("Phase II Membership"), choices = names(data()))
   })
   
   output$weights<- renderUI({
     selectInput("weights", label = h5("Weights"), choices = names(data()))
   })
   
-  
-  output$cal <- renderUI({
-    selectInput("cal", label = h5("Calibration variables"), choices = names(data()), multiple = TRUE)
+  output$p2probs <- renderUI({
+    selectInput("p2probs", label = h5("Phase II Subsampling Probabilities"), choices = names(data()))
+                
   })
   
-# output$regTab <- renderTable({
+  output$cal <- renderUI({
+    selectInput("cal", label = h5("Calibration Variables"), choices = names(data()), multiple = TRUE,
+                selected = NULL)
+  })
+  
+# output$regTab <- renderTable({    
 #    inFile <- input$file1
-#  if (input$Sampling == "Random Sampling"){
-     #data <- read.csv(inFile$datapath, header=input$header, sep=input$sep, quote=input$quote) 
- #   fit1 <- ah(Surv(input$surv, input$cen) ~ input$covariates, data = data )
+#    if (input$Sampling == "Random Sampling"){
+#    data <- read.csv(inFile$datapath, header=input$header, sep=input$sep, quote=input$quote)
+#    fit1 <- ah(Surv(input$surv, input$cen) ~ input$covariates, data = data)
 #     summary(fit1)
-#   }
-    
-    
-#  })
+#    }
+# })
 
 # output$plot <- renderPlot({
 #   dat <- get(input$dataset)
 #   hist(input$surv)
 # })
 
-
-runRegression <- reactive({
-  #f1<-ah(as.formula(paste("Surv(", input$surv,",", input$cen,") ~ ",paste(input$covariates,collapse="+"))),data=data, ties=FALSE)
-  #mysummary(f1)$coef
+  ## "Fit Model" button
+  observeEvent(input$fitModel, {
   
-  lm(as.formula(paste(input$surv," ~ ",paste(input$covariates,collapse="+"))),data=data)
+      inFile <- input$file1
+      dat <- read.csv(inFile$datapath, header=input$header, sep=input$sep, quote=input$quote)
+    
+      ## regular Cox model
+      #fit1 <- coxph(as.formula(paste("Surv(", input$surv,",", input$cen,") ~ ",paste(input$covariates,collapse="+"))), data=dat)
+      
+      ## additive hazards model
+      if (input$twophase==F){
+         #if (input$wgts==T){
+         #  dat$w <- dat[, unlist(input$weights)]
+         #  fit1 <-ah(as.formula(paste("Surv(", input$surv,",", input$cen,") ~ ",paste(input$covariates,collapse="+"))),
+         #       data=dat, robust=input$robust, ties=input$ties, weights=w)
+         #} else {
+           fit1 <-ah(as.formula(paste("Surv(", input$surv,",", input$cen,") ~ ",paste(input$covariates,collapse="+"))),
+                     data=dat, robust=input$robust, ties=input$ties)
+         #}
+      ## additive hazards model with 2-phase sampling
+      } else {
+         dat$R <- dat[, unlist(input$R)]            
+         dat$Pi <- dat[, unlist(input$p2probs)]
+        
+         ## currently with no consideration of calibration variables
+         fit1 <-ah.2ph(as.formula(paste("Surv(", input$surv,",", input$cen,") ~ ",paste(input$covariates,collapse="+"))),
+                       data=dat, robust=input$robust, R=R, Pi=Pi)
+    
+      }
+      
+      # ah() options: robust = T/F; weights; ties = T/F
+      # ah.2ph() options: robust = T/F; R (indicator for being in phase 2; Pi: Pr(subject selected to phase 2 subsample)
+      # calibration.variables: used to calibrate the weights
+      
+      # output$modelSummary <- renderPrint({   
+      #     summary(fit1)  ## for checking purposes
+      # })
+      
+      output$regTab <- renderTable({
+          # headings: coef	 se	 lower.95	 upper.95	 z	 p.value
+          summary(fit1)$coef
+      }, caption = "Table 1. Regression Output")
+      
   
-  })
+  }) # end observeEvent
+  
+}) ## end document
 
-output$regTab <- renderTable({
-  if(!is.null(input$covariates)){
-    summary(runRegression())$coefficients
-  } else {
-    print(data.frame(Warning="Please select Model Parameters."))
-  }
- })
+# runRegression <- reactive({
+#   #f1<-ah(as.formula(paste("Surv(", input$surv,",", input$cen,") ~ ",paste(input$covariates,collapse="+"))),data=data, ties=FALSE)
+#   #mysummary(f1)$coef
+#   
+#   lm(as.formula(paste(input$surv," ~ ",paste(input$covariates,collapse="+"))),data=data)
+#   
+#   })
+
+# output$regTab <- renderTable({
+#   if(!is.null(input$covariates)){
+#     summary(runRegression())$coefficients
+#   } else {
+#     print(data.frame(Warning="Please select Model Parameters."))
+#   }
+#  })
 
 
 
@@ -120,4 +196,3 @@ output$regTab <- renderTable({
  # fit1.2<-ah.2ph(formula,data=aric.LDL, R=aric.LDL$R, Pi=1/aric.LDL$wts, robust=robust,ties=FALSE)
   #su
   
-})
--- ui.R
+++ ui.R
@@ -7,24 +7,35 @@ ui <- dashboardPage(
                   # Model for Survival Analysis"),
   dashboardSidebar(
    sidebarMenu(
-      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-      menuItem("Data", tabName = "data", icon = icon("database")),
-      menuItem("Model", tabName = "model", icon = icon("yahoo")),
-      menuItem("Sampling", tabName = "sampling", icon = icon("bitbucket")),
-      menuItem("Results", tabName = "results", icon = icon("magic")),
-      menuItem("Plots", tabName = "plots", icon = icon("line-chart"))
+      menuItem(strong("Dashboard", style="font-size: 14pt"), tabName = "dashboard", icon = icon("dashboard")),
+      menuItem(strong("Data", style="font-size: 14pt"), tabName = "data", icon = icon("database")),
+      menuItem(strong("Model", style="font-size: 14pt"), tabName = "model", icon = icon("yahoo")),
+     # menuItem("Sampling", tabName = "sampling", icon = icon("bitbucket")),
+      menuItem(strong("Results", style="font-size: 14pt"), tabName = "results", icon = icon("magic")),
+      menuItem(strong("Plots", style="font-size: 14pt"), tabName = "plots", icon = icon("line-chart"))
 
-    )
-  ),
+  )),
   dashboardBody(
     tabItems(
       # First tab content
-      tabItem(tabName = "dashboard"),
+      tabItem(tabName = "dashboard",
+                fluidPage(
+                  titlePanel("Introduction"),
+                  
+                  p("This web app is a tool for fitting single- and two-phase additive hazards models, 
+                          estimating parameter estimates and their model-based or robust standard errors, and  
+                          visualizing predicted individual-specific hazards.", 
+                          style = "font-family: 'Calibri'; font-size: 14pt"),
+                  
+                  p("Documentation for the associated R package may be found", 
+                  a("here.", href="https://cran.r-project.org/web/packages/addhazard/addhazard.pdf"), 
+                    style = "font-family: 'Calibri'; font-size: 14pt")
+                )),
 
       # Second tab content
       tabItem(tabName = "data",
               fluidPage(
-                titlePanel("Uploading Files"),
+                titlePanel("Data Upload"),
 
                 tags$head( 
                   tags$link(rel = "stylesheet", type = "text/css", href="ah.css")
@@ -32,22 +43,23 @@ ui <- dashboardPage(
                 
                   sidebarPanel(
                     
-                    fileInput('file1', 'Choose CSV File',
-                              accept=c('text/csv',
-                                       'text/comma-separated-values,text/plain',
-                                       '.csv')),
-                    tags$hr(),
-                    checkboxInput('header', 'Header', TRUE),
-                    radioButtons('sep', 'Separator',
+                   tags$b("Set File Options"),
+                    checkboxInput('header', 'Variable names as headers', TRUE),
+                    radioButtons('sep', 'Field separator character',
                                  c(Comma=',',
                                    Semicolon=';',
                                    Tab='\t'),
                                  ','),
-                    radioButtons('quote', 'Quote',
+                    radioButtons('quote', 'Quoting characters',
                                  c(None='',
                                    'Double Quote'='"',
                                    'Single Quote'="'"),
-                                 '"')
+                                 '"'),
+                    fileInput('file1', 'Choose File (.csv, .xlsx, .txt, .dta, .sav)',
+                              accept=c('text/csv',
+                                       'text/comma-separated-values,text/plain',
+                                       '.csv')),
+                    tags$hr()
                   ),
               
                   mainPanel(dataTableOutput('contents'))
@@ -62,31 +74,61 @@ ui <- dashboardPage(
                   uiOutput("surv"),
                   uiOutput("cen"),
                   uiOutput("covariates"),
-                  checkboxInput('robust', 'Robust Standard Errors', TRUE)
+                  #checkboxInput('wgts', 'Weights', FALSE),
+                  #uiOutput("weights"),
+                  checkboxInput('robust', 'Robust Standard Errors', TRUE),
+                  
+                  # from Sampling tab
+                  # selectInput("Sampling", "Sampling Scheme:",
+                  #            choices=c("Random Sampling", "Two-phase Sampling")),
+                  #h6("If two-phase sampling:"),
+
+                  #uiOutput("phase1"),
+                  #uiOutput("phase2"),
+                  checkboxInput('ties', 'Ties', TRUE),
+                  
+                  checkboxInput('twophase', 'Two-Phase Sampling', FALSE),
+                  helpText("Uncheck if records were randomly sampled."),
+                  
+                  uiOutput("R"),
+                  uiOutput("p2probs"),
+                  #checkboxInput('calibration', 'Calibration', FALSE),
+                  # helpText("Sometimes you need to create new calibration variables based on phase I variables.
+                  #          The key is to find the variables highly correlated with phase II variable "),
+                  #uiOutput("cal"),
+                  #helpText("The inverse of the phase II selection probability for each subject. 
+                  #         It is a number greater than or equal to 1."),
+                  
+                  actionButton("fitModel", "Fit Model")  
+                  # verbatimTextOutput("modelSummary")  
                 ),
                   mainPanel(
                     tableOutput("regTab")
                     #textOutput("text1")
                     )               
-              )
-      ),
+              ) # end fluidPage
+      ), # end tabItem
       
       ## fourth tab 
       tabItem(tabName ="Two-phase Sampling",
               fluidPage(
                 #  selectInput("Sampling", "Sampling Scheme:",
                 #            choices=c("Random Sampling", "Two-phase Sampling")),
-                #  h6("If two-phase sampling:"), 
+                #  h6("If two-phase sampling:"),
+
                   uiOutput("phase1"),
-                  uiOutput("phase2"),
-                  uiOutput("R"),
-                  uiOutput("weights"), 
-                  helpText("The inverse of the phase II selection probability for each subject. It is a number greater or equal to 1."),
-                  checkboxInput('calibration', 'Calibration', TRUE),
-                  uiOutput("cal"),
-                  helpText("Sometimes you need to create new calibration variables based on phase I variables. The key is to find the variables highly correlated with phase II variable ")
-               )
-              ), 
+                  uiOutput("phase2")#,
+                 # uiOutput("R"),
+                 # uiOutput("weights"),
+                 # helpText("The inverse of the phase II selection probability for each subject. It is a number greater or equal to 1."),
+                  # checkboxInput('calibration', 'Calibration', TRUE),
+                  # uiOutput("cal"),
+                  #helpText("Sometimes you need to create new calibration variables based on phase I variables.
+                  #         The key is to find the variables highly correlated with phase II variable ")
+
+              ) # end fluidPage
+          ), # end tabItem
+
       ## fifth tab
       tabItem( tabName = "results",
         fluidPage(
@@ -100,19 +142,12 @@ ui <- dashboardPage(
             #  )
             #)
           )
-         )
-       )
+         ) # endfluidPage
+       ) # end tabItem
      
-              
-        
-              
-              
-              
-              
-      
-    )
-  )
-)
+    ) # end tabItems
+  ) # end dashboardBody
+) # end dashboardPage
 
 
 
